4f0c4b3caea5c51be2bf0ad9c3511479ba1d5a40
Anthony Kalkhorani <seek.and.you.wil.find02@gmail.com>
2022-03-28 10:43:27 +0200

    fixed small error thing in index file, added add form and made some changes to the DishController.java class

--- src/main/java/com/example/foodmenu/controller/DishController.java
+++ src/main/java/com/example/foodmenu/controller/DishController.java
@@ -5,10 +5,7 @@ import com.example.foodmenu.service.DishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,7 +23,7 @@ public class DishController {
     }
 
     @PostMapping("/saveDish")
-    public String saveDish(Dish dish) {
+    public String saveStudent(@ModelAttribute("dish") Dish dish) {
         dishService.addDish(dish);
         return "redirect:/index";
     }
