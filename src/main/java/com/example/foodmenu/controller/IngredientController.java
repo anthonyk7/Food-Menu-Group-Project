1fa06cfb361340925b30510dff01a051608bdc51
Lazereel <emil.leven@iths.se>
2022-04-01 10:36:19 +0200

    more ingr

--- src/main/java/com/example/foodmenu/controller/IngredientController.java
+++ src/main/java/com/example/foodmenu/controller/IngredientController.java
@@ -34,14 +34,15 @@ public class IngredientController {
         model.addAttribute("ingredient", ingredient);
         List<Ingredient> ingredients = ingredientService.findAllIngredients();
         model.addAttribute("ingredientList", ingredients);
+
         return "ingredients-form";
     }
+
 
     @GetMapping("/popDbIngridients")
     public String populateDatabase() {
         Ingredient endive = new Ingredient();
         endive.setName("Endive");
-
         saveIngredient(endive);
 
 
@@ -154,29 +155,47 @@ public class IngredientController {
         Ingredient carrot = new Ingredient();
         carrot.setName("Carrot");
 
+        saveIngredient(carrot);
+
         Ingredient tomato = new Ingredient();
         tomato.setName("Tomato");
+
+        saveIngredient(tomato);
 
         Ingredient garlic = new Ingredient();
         garlic.setName("Garlic");
 
+        saveIngredient(garlic);
+
         Ingredient paprika = new Ingredient();
         paprika.setName("Paprika");
+
+        saveIngredient(paprika);
 
         Ingredient parsley = new Ingredient();
         parsley.setName("Parsley");
 
+        saveIngredient(parsley);
+
         Ingredient scallop = new Ingredient();
         scallop.setName("Scallop");
+
+        saveIngredient(scallop);
 
         Ingredient chorizoOil = new Ingredient();
         chorizoOil.setName("Chorizo Oil");
 
+        saveIngredient(chorizoOil);
+
         Ingredient marconaAlmond = new Ingredient();
         marconaAlmond.setName("Marcona Almonds");
 
+        saveIngredient(marconaAlmond);
+
         Ingredient chickpea = new Ingredient();
         chickpea.setName("Chickpea");
+
+        saveIngredient(chickpea);
 
         Ingredient chuckroll = new Ingredient();
         chuckroll.setName("Chuck Roll");
@@ -184,122 +203,192 @@ public class IngredientController {
         Ingredient thyme = new Ingredient();
         thyme.setName("Thyme");
 
+        saveIngredient(thyme);
+
         Ingredient schallot = new Ingredient();
         schallot.setName("Schallots");
+
+        saveIngredient(schallot);
 
         Ingredient redBurgundy = new Ingredient();
         redBurgundy.setName("Red Burgundy");
 
+        saveIngredient(redBurgundy);
+
         Ingredient potato = new Ingredient();
         potato.setName("Potato");
+
+        saveIngredient(potato);
 
         Ingredient heavyCream = new Ingredient();
         heavyCream.setName("Heavy Cream");
 
+        saveIngredient(heavyCream);
+
         Ingredient milk = new Ingredient();
         milk.setName("Milk");
+
+        saveIngredient(milk);
 
         Ingredient bouqetGarni = new Ingredient();
         bouqetGarni.setName("Bouqet Garni");
 
+        saveIngredient(bouqetGarni);
+
         Ingredient saffron = new Ingredient();
         saffron.setName("Saffron");
+
+        saveIngredient(saffron);
 
         Ingredient whiteWine = new Ingredient();
         whiteWine.setName("White Wine");
 
+        saveIngredient(whiteWine);
+
         Ingredient cauliflower = new Ingredient();
         cauliflower.setName("Cauliflower");
+
+        saveIngredient(cauliflower);
 
         Ingredient halibut = new Ingredient();
         halibut.setName("Halibut");
 
+        saveIngredient(halibut);
+
         Ingredient haricotVert = new Ingredient();
         haricotVert.setName("Haricot Vert");
+
+        saveIngredient(haricotVert);
 
         Ingredient fishStock = new Ingredient();
         fishStock.setName("Fish Stock");
 
+        saveIngredient(fishStock);
+
         Ingredient foieGras = new Ingredient();
         foieGras.setName("Foie Gras");
+
+        saveIngredient(foieGras);
 
         Ingredient redOnion = new Ingredient();
         redOnion.setName("Red Onion");
 
-        Ingredient sugar = new Ingredient();
-        sugar.setName("Sugar");
+        saveIngredient(redOnion);
 
         Ingredient baguette = new Ingredient();
         baguette.setName("Baguette");
 
+        saveIngredient(baguette);
+
         Ingredient artichoke = new Ingredient();
         artichoke.setName("Artichoke");
+
+        saveIngredient(artichoke);
 
         Ingredient vegetableStock = new Ingredient();
         vegetableStock.setName("Vegetable Stock");
 
+        saveIngredient(vegetableStock);
+
         Ingredient tuna = new Ingredient();
         tuna.setName("Tuna");
+
+        saveIngredient(tuna);
 
         Ingredient olive = new Ingredient();
         olive.setName("Olives");
 
+        saveIngredient(olive);
+
         Ingredient duck = new Ingredient();
         duck.setName("Duck");
+
+        saveIngredient(duck);
 
         Ingredient orange = new Ingredient();
         orange.setName("Orange");
 
+        saveIngredient(orange);
+
         Ingredient chickenStock = new Ingredient();
         chickenStock.setName("Chicken Stock");
+
+        saveIngredient(chickenStock);
 
         Ingredient zucciniBlossom = new Ingredient();
         zucciniBlossom.setName("Zuccini Blossoms");
 
+        saveIngredient(zucciniBlossom);
+
         Ingredient watercress = new Ingredient();
         watercress.setName("Water Cress");
+
+        saveIngredient(watercress);
 
         Ingredient cremeCheese = new Ingredient();
         cremeCheese.setName("Creme Cheese");
 
+        saveIngredient(cremeCheese);
+
         Ingredient puffPastry = new Ingredient();
         puffPastry.setName("Puff Pastry");
+
+        saveIngredient(puffPastry);
 
         Ingredient egg = new Ingredient();
         egg.setName("Egg");
 
+        saveIngredient(egg);
+
         Ingredient tenderloin = new Ingredient();
         tenderloin.setName("Tenderloin");
+
+        saveIngredient(tenderloin);
 
         Ingredient smokedPork = new Ingredient();
         smokedPork.setName("Smoked Pork");
 
+        saveIngredient(smokedPork);
+
         Ingredient champignon = new Ingredient();
         champignon.setName("Champignon");
+
+        saveIngredient(champignon);
 
         Ingredient flour = new Ingredient();
         flour.setName("Flour");
 
+        saveIngredient(flour);
+
         Ingredient vanillaSugar = new Ingredient();
         vanillaSugar.setName("Vanilla Sugar");
+
+        saveIngredient(vanillaSugar);
 
         Ingredient strawberry = new Ingredient();
         strawberry.setName("Strawberry");
 
+        saveIngredient(strawberry);
+
         Ingredient chocolate = new Ingredient();
         chocolate.setName("Chocolate");
+
+        saveIngredient(chocolate);
 
         Ingredient babySquid = new Ingredient();
         babySquid.setName("Baby Squid");
 
+        saveIngredient(babySquid);
+
         Ingredient lolloRosso = new Ingredient();
         lolloRosso.setName("Lollo Rosso");
 
-        Ingredient lolloBiondo = new Ingredient();
-        lolloBiondo.setName("Lollo Biondo");
+        saveIngredient(lolloRosso);
 
         Ingredient boneMarrowBone = new Ingredient();
         boneMarrowBone.setName("Bone Marrow");
+
+        saveIngredient(boneMarrowBone);
 
         Ingredient sourDoughBread = new Ingredient();
         sourDoughBread.setName("Sourdough Bread");
